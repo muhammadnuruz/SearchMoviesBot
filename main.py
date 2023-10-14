ceebe49734248aee96e25b2cffb1b94502aff0a0
muhammadnur <muhammadnurpdp@gmail.com>
2023-10-14 08:58:28 +0500

    added alembic upgrade

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ async def create_all():
 
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.ERROR)
+    logging.basicConfig(level=logging.INFO)
     loop = asyncio.get_event_loop()
     loop.run_until_complete(create_all())
     executor.start_polling(dp, skip_updates=True)
