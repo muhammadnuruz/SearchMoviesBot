7e0ebf15e9bcb93ac196cf92450a29a62d76550f
muhammadnur <muhammadnurpdp@gmail.com>
2023-10-15 09:41:04 +0500

    Added other handlers

--- bot/buttons/reply_buttons.py
+++ bot/buttons/reply_buttons.py
@@ -1,13 +1,14 @@
 from aiogram.types import ReplyKeyboardMarkup
 
 from bot.buttons.text import movie, back_main_menu, admins_txt, statistic, advert, sub_admins, add_admins, add_movie, \
-    sub_movie, movie_setting, cabinet, vip, add_money
+    sub_movie, movie_setting, cabinet, vip, add_money, advert_and_support
 
 
 async def main_menu_buttons():
     design = [
-        [cabinet, movie],
-        [vip, add_money]
+        [movie],
+        [vip, cabinet],
+        [add_money, advert_and_support]
     ]
     return ReplyKeyboardMarkup(keyboard=design, resize_keyboard=True)
 
--- bot/handlers/other_handlers.py
+++ bot/handlers/other_handlers.py
@@ -1,7 +1,7 @@
 from aiogram import types
 from aiogram.dispatcher.filters import Text
 
-from bot.buttons.text import cabinet, vip, add_money
+from bot.buttons.text import cabinet, vip, add_money, info, advert_and_support
 from bot.dispatcher import dp
 
 
@@ -23,4 +23,14 @@ async def vip_function(msg: types.Message):
 
 @dp.message_handler(Text(add_money))
 async def add_money_function(msg: types.Message):
+    await msg.answer(text=f"Tez orada ishga tushadi❗️")
+
+
+@dp.message_handler(Text(info))
+async def add_info_function(msg: types.Message):
+    await msg.answer(text=f"Tez orada ishga tushadi❗️")
+
+
+@dp.message_handler(Text(advert_and_support))
+async def add_advert_function(msg: types.Message):
     await msg.answer(text=f"Tez orada ishga tushadi❗️")
